a3d5b80f546dc4c5b03f5099b33b40f5bc4262c1
NoFace92500 <raymond.bilodeau95@gmail.com>
2023-03-23 19:47:06 -0400

    pushing project to github

---
+++ code.js
@@ -0,0 +1,11 @@
+/*
+Puesodo code
+Step 1 create the variable combo1, combo2, combo3
+Step 2 Set the alert to display the welcome message and vault combination  
+*/
+//creates and sets each of the 3 combination numbers
+const combo1 = 25 - 15 //10
+const combo2 = 10 * 10 - 60 //40
+const combo3 = (55 + 23) / 2 //39
+// creates the pop up dialog box message
+alert("Welcome, you have been selected for a secret message to open the Vault. The combo is: " + combo1 + "-" + combo2 + "-" + combo3);
